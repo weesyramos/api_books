b336287331734c432f6b2678524fbfdfabed04aa
weslley <weesy.ramos14@gmail.com>
2020-02-28 19:26:40 -0300

    config settings.py staticfiles

--- api/settings.py
+++ api/settings.py
@@ -109,10 +109,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+# SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+# STATICFILES_DIRS = (
+#   os.path.join(SITE_ROOT, 'static/'),
+# )
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+SITE_ROOT = os.path.dirname(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static/'),
+  os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
