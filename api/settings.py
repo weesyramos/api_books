45d237025926d65cae2cf9b7e4d9a06b57d17687
weslley <weesy.ramos14@gmail.com>
2020-03-02 23:05:30 -0300

    config settings statics

--- api/settings.py
+++ api/settings.py
@@ -15,10 +15,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1, ''0.0.0.0', 'api-olist-books.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'api-olist-books.herokuapp.com']
 
 
 
