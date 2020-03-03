6b14a9f04f1219bae073c0062dce02ca1650fb03
weslley <weesy.ramos14@gmail.com>
2020-03-03 00:32:43 -0300

    teste

--- api/wsgi.py
+++ api/wsgi.py
@@ -10,11 +10,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 # application = Cling(get_wsgi_application())
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
